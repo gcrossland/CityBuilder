4b73a6bdc55847850ce35a084525d6c09906e74b
gcrossland <none@none>
2017-09-17 15:37:40 +0100

    Added animation support to xpm-to-gif

--- xpm-to-gif.sh
+++ xpm-to-gif.sh
@@ -10,14 +10,51 @@ done
 
 gimp --new-instance --no-data --no-fonts --batch-interpreter python-fu-eval --batch - --batch "pdb.gimp_quit(1)" <<END
 # -*- coding: $(locale charmap) -*-
+import sys
+
+def exit (v):
+  sys.stderr.write(v + "\n")
+  pdb.gimp_quit(1)
+
 args = u"${argStr}".split(u"\uFFFF")[:-1]
 
-for inPathName in args:
-  outPathName = inPathName + ".gif"
-  i = pdb.file_xpm_load(inPathName, inPathName)
+syntax = "Syntax: xpm-to-gif <frames per second>|0 [...]"
+if len(args) < 1:
+  exit(syntax)
+try:
+  frameRate = float(args[0])
+except ValueError as e:
+  exit("Frame duration must be a number\n" + syntax)
+if frameRate < 0:
+  exit("Frame duration must be a positive number\n" + syntax)
+
+def loadXpm (pathName):
+  i = pdb.file_xpm_load(pathName, pathName)
   #pdb.gimp_image_scale_full(i, i.width * 1, i.height * 1, 0)
-  #pdb.file_png_save_defaults(i, i.active_layer, outPathName, outPathName)
   pdb.gimp_image_convert_indexed(i, 0, 0, 256, 0, 1, "")
-  pdb.file_gif_save(i, i.active_layer, outPathName, outPathName, 0, 0, 1, 0)
-  pdb.gimp_image_delete(i)
+  return i
+
+if frameRate == 0:
+  for inPathName in args[1:]:
+    i = loadXpm(inPathName)
+    outPathName = inPathName + ".gif"
+    pdb.file_gif_save(i, i.active_layer, outPathName, outPathName, 0, 0, 1, 0)
+    pdb.gimp_image_delete(i)
+else:
+  if len(args) > 1:
+    i = loadXpm(args[1])
+
+    for inPathName in args[2:]:
+      layer = i.new_layer()
+
+      i1 = loadXpm(inPathName)
+      pdb.gimp_edit_copy(i1.active_layer)
+      pdb.gimp_image_delete(i1)
+
+      selection = pdb.gimp_edit_paste(layer, True)
+      pdb.gimp_floating_sel_anchor(selection)
+
+    outPathName = args[1] + ".gif"
+    pdb.file_gif_save(i, i.active_layer, outPathName, outPathName, 0, 0, 1000 / frameRate, 0)
+    pdb.gimp_image_delete(i)
 END
